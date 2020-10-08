4e41633b42a3390a0513fe718838f1077d807aca
Monika <monikaponagani@gmail.com>
2020-10-08 17:55:56 -0400

    sum.java V2

--- sum.java
+++ sum.java
@@ -5,8 +5,9 @@ class sum
 		int a,b,c;
 		a=50;
 		b=20;
-		c=a+b;
-		System.out.println("the sum of three values are" +c);
+		c=a+b+d;
+		int d;
+		System.out.println("the sum of three values is"+c);
 	}
 }
 
